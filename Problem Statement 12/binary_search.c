034aad1a3518ff7bbb4195424e9a80e1b8b45578
PiyushSawarkar <44064862+PiyushSawarkar@users.noreply.github.com>
2019-04-28 10:34:12 +0530

    Update binary_search.c

--- Problem Statement 12/binary_search.c
+++ Problem Statement 12/binary_search.c
@@ -1,54 +1,34 @@
-#include<stdio.h>
-int a[80];
-void bubbleSort(int a[],int n){
-int i,j,t;
-for(i=0;i<n;i++){
-for(j=0;j<n-1-i;j++){
-if(a[j]>a[j+1]){
-t=a[j];
-a[j]=a[j+1];
-a[j+1]=t;
+#include <stdio.h>
+void binary_search();
+ 
+int a[50], n, item, loc, beg, mid, end, i;
+void main()
+{
+    printf("\nEnter size of an array: ");
+    scanf("%d", &n);
+    printf("\nEnter elements of an array in sorted form:\n");
+    for(i=0; i<n; i++)
+        scanf("%d", &a[i]);
+    printf("\nEnter ITEM to be searched: ");
+    scanf("%d", &item);
+    binary_search();
+    getch();
 }
-}
-}
-for(i=0;i<n;i++){
-printf("%d ",a[i]);
-}
-}
-int main(void){
-int n,i,first,last,middle,pos=-1,val;
-printf("\nEnter Number of values = ");
-scanf("%d",&n);
-printf("\nEnter Elements :\n");
-first=0;
-last=n-1;
-middle=(first+last)/2;
-for(i=0;i<n;i++){
-scanf("%d",&a[i]);
-}
-printf("\nenter the value to be searched = ");
-scanf("%d",&val);
-bubbleSort(a,n);
-while(first<=last){
-if(val>a[middle]){
-first=middle+1;
-last=n-1;
-middle=(first+last)/2;
-}
-else if(val<a[middle]){
-first=0;
-last=middle-1;
-middle=(first+last)/2;
-}
-else if(val==a[middle]){
-pos=middle;
-printf("\nValue found at Position %d and is equal
-to %d",pos,val);
-break;
-}
-}
-if(pos==-1){
-printf("\nValue Not Present");
-}
-return(0);
+void binary_search()
+{
+    beg = 0;
+    end = n-1;
+    mid = (beg + end) / 2;
+    while ((beg<=end) && (a[mid]!=item))
+    {
+        if (item < a[mid])
+            end = mid - 1;
+        else
+            beg = mid + 1;
+        mid = (beg + end) / 2;
+    }
+    if (a[mid] == item)
+        printf("\n\nITEM found at location %d", mid+1);
+    else
+        printf("\n\nITEM doesn't exist");
 }
